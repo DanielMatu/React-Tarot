92e7428d1ae36802cd89f1cbce81843ba554191c
matudani <daniel.matu@mail.utoronto.ca>
2021-06-26 19:16:17 -0400

    init

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingPage = () => (
+    <div className="loader">
+        <img className="loader__image" src="https://acegif.com/wp-content/uploads/loading-25.gif"></img>
+    </div>
+)
+
+export default LoadingPage;
