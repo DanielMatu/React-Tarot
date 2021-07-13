df24e439f32ebb841052508990c51d51aa8b0774
matudani <daniel.matu@mail.utoronto.ca>
2021-07-13 00:17:15 -0400

    added static create page

--- src/components/calendar/CreateJournalEntryPage.js
+++ src/components/calendar/CreateJournalEntryPage.js
@@ -6,16 +6,32 @@ import { getMonthAndNumDays } from '../../actions/calendarUpdatingFuncs'
 const CreateJournalEntryPage = () => {
     let [state, monthInc, monthDec, yearInc, yearDec] = useContext(DateContext)
     let [ numericalMonth, year, calendar ] = state
-    let [ month, numDays ] = getMonthAndNumDays(numericalMonth) 
-
+    let [ stringMonth, numDays ] = getMonthAndNumDays(numericalMonth) 
+    let entryTitle = "entry title"
+    let entryDate = "Jan 8 920"
+    let entryText = 'here is some text'
 
     return (
-        <div>
+        <div className = 'create-entry-wrapper'>
+            <div className='create-entry-container'>
+                <div className='half-entry text-entry'>
+                    <textarea className='entry-text-area entry-title' value={entryTitle}></textarea>
+                    <textarea className='entry-text-area entry-date' value={entryDate}></textarea>
 
+                    <textarea className='entry-text-area text-area-body' >
 
-                cjep
-
-
+                    </textarea>
+                </div>
+                <div className='book-spine'></div>
+                <div className='half-entry bottom-entry'>
+                    <div className='entry-button view-fortune-button'>
+                        VIEW FORTUNE
+                    </div>
+                    <div className='entry-button save-button'>
+                        SAVE
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
