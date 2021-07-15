b562e12fc3f5372b66645c1e22a43d3752ea830f
matudani <daniel.matu@mail.utoronto.ca>
2021-07-15 17:31:53 -0400

    gonna switch fortune from state to context

--- src/components/FortunePage.js
+++ src/components/FortunePage.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import { history } from '../routers/AppRouter'
+import { getStandardDeck } from '../decks/StandardDeck'
+import { pullNCards } from '../decks/DeckHelpers'
 
 const FortunePage = () => {
-    const [layout, setLayout] = useState([])
-    const [displayCard, setDisplayCard] = useState({imgUrl: 'url("../../public/images/Tarot/ace of cups.png")'})
+    const deck = getStandardDeck()
+    let nineCards = pullNCards(9, deck)
+    const [layout, setLayout] = useState(nineCards)
+    const [displayCard, setDisplayCard] = useState({name: 'ace of cups'})
     const [displayText, setDisplayText] = useState()
     const updateText = (e) => setDisplayText(e.target.value)
     return (
@@ -12,7 +16,7 @@ const FortunePage = () => {
             </div>
             <div className='display-section'>
                 <div className='display-card-container'>
-                    <div className='display-card' style={{backgroundImage: displayCard.imgUrl, backgroundSize:'cover'}}>
+                    <div className='display-card' style={{backgroundImage: 'url("../../public/images/Tarot/' + displayCard.name + '.png")', backgroundSize:'cover'}}>
                     </div>
                 </div>
                 <textarea className='display-text' value={displayText} onChange={updateText}>
---
+++ src/decks/DeckHelpers.js
@@ -0,0 +1,11 @@
+export const pullNCards = (n, deck) => {
+    let nCards = []
+    let len = deck.length 
+    let randomIndex
+    for (let i = 0; i < n; i++){
+        randomIndex = Math.floor(Math.random()*(len--))
+        nCards.push(deck.splice(randomIndex, 1))
+
+    }
+    return nCards
+}
